396b14342db90175728bb30ddef804cd4ae19f65
Chisom <chisomije92@gmail.com>
2022-05-29 16:57:31 +0100

    correct writefilesync

--- server.ts
+++ server.ts
@@ -3,29 +3,27 @@ import http from "http";
 import fs from "fs";
 
 const server = http.createServer((req, res) => {
-  //   console.log(req.url, req.method, req.headers);
-
   const url = req.url;
   const method = req.method;
   if (url === "/") {
     res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
+    res.write("<head><title>Enter Message</title><head>");
     res.write(
-      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Submit</button></form></body>"
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
     );
     res.write("</html>");
     return res.end();
   }
   if (url === "/message" && method === "POST") {
     const body: any = [];
-    res.on("data", (chunk) => {
+    req.on("data", (chunk) => {
+      console.log(chunk);
       body.push(chunk);
     });
-    res.on("end", () => {
+    req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split("=")[1];
-      fs.writeFileSync("sample.txt", message);
-      console.log(message);
+      fs.writeFileSync("message.txt", message);
     });
     res.statusCode = 302;
     res.setHeader("Location", "/");
@@ -33,7 +31,7 @@ const server = http.createServer((req, res) => {
   }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
+  res.write("<head><title>My First Page</title><head>");
   res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
   res.write("</html>");
   res.end();
