9c075f2a93f0e1d7aa38df5a73d665a806a94c0a
Chisom <chisomije92@gmail.com>
2022-05-29 16:15:22 +0100

    rout and redirect requests

--- server.ts
+++ server.ts
@@ -1,8 +1,25 @@
 import http from "http";
 // const http = require('http');
+import fs from "fs";
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  //   console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Submit</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && req.method === "POST") {
+    fs.writeFileSync("message.txt", "dummy message");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My First Page</title></head>");
