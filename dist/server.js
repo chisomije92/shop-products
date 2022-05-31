84f5bec485b1893dc8271774184229137b4a3ae9
Chisom <chisomije92@gmail.com>
2022-05-31 12:47:45 +0100

    add body-parser

--- dist/server.js
+++ dist/server.js
@@ -1,13 +1,16 @@
-import http from "http";
 import express from "express";
+import bodyParser from "body-parser";
 const app = express();
-app.use((req, res, next) => {
-    console.log("Middleware 1");
-    next();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/add-product", (req, res) => {
+    res.send('<body><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form></body>');
 });
-app.use((req, res, next) => {
-    console.log("Middleware 2");
+app.use("/product", (req, res, next) => {
+    console.log(req.body);
+    res.redirect("/");
 });
-const server = http.createServer(app);
-server.listen(3000);
+app.use("/", (req, res, next) => {
+    res.send("<h1>Hello from Express!</h1>");
+});
+app.listen(3000);
 //# sourceMappingURL=server.js.map
