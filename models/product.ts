bcf55a209b8cda2d198f065190966d4914f748a8
Chisom <chisomije92@gmail.com>
2022-06-03 11:34:08 +0100

    inject and output dynamic data

--- models/product.ts
+++ models/product.ts
@@ -21,8 +21,19 @@ const getProductsFromFile = (cb: (products: ProductType[]) => void) => {
 
 export class Product {
   title: string;
-  constructor(title: string) {
+  imageUrl: string;
+  description: string;
+  price: number;
+  constructor(
+    title: string,
+    imageUrl: string,
+    description: string,
+    price: number
+  ) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
