eed5d8e01375d2c39cb49e7c73088c0a67466866
Chisom <chisomije92@gmail.com>
2022-06-09 10:15:27 +0100

    implement connection to database

--- dist/utils/database.js
+++ dist/utils/database.js
@@ -1,9 +1,15 @@
-import mysql2 from "mysql2";
-const pool = mysql2.createPool({
+// import mysql2 from "mysql2";
+import Sequelize from "sequelize";
+// const pool = mysql2.createPool({
+//   host: "localhost",
+//   user: "root",
+//   database: "node-complete",
+//   password: "jerry",
+// });
+// export default pool.promise();
+const sequelize = new Sequelize("node-complete", "root", "jerry", {
+    dialect: "mysql",
     host: "localhost",
-    user: "root",
-    database: "node-complete",
-    password: "jerry",
 });
-export default pool.promise();
+export default sequelize;
 //# sourceMappingURL=database.js.map
--- utils/database.ts
+++ utils/database.ts
@@ -1,10 +1,17 @@
-import mysql2 from "mysql2";
+// import mysql2 from "mysql2";
+import Sequelize from "sequelize";
+// const pool = mysql2.createPool({
+//   host: "localhost",
+//   user: "root",
+//   database: "node-complete",
+//   password: "jerry",
+// });
 
-const pool = mysql2.createPool({
+// export default pool.promise();
+
+const sequelize = new Sequelize("node-complete", "root", "jerry", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "jerry",
 });
 
-export default pool.promise();
+export default sequelize;
