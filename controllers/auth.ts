77c6658f830d6ccbd6fc0c4b9a43e5d0669df4a9
Chisom <chisomije92@gmail.com>
2022-06-20 17:09:19 +0100

    implement setting cookie

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -1,14 +1,15 @@
 import { Request, Response, NextFunction } from "express";
 
 export const getLogin = (req: Request, res: Response, next: NextFunction) => {
+  const isLoggedIn = req.get("Cookie")?.split("=")[1] === "true";
   res.render("auth/login", {
     pageTitle: "Login",
     path: "/login",
-    isAuthenticated: false,
+    isAuthenticated: isLoggedIn,
   });
 };
 
 export const postLogin = (req: Request, res: Response, next: NextFunction) => {
-  req.isLoggedIn = true;
+  res.setHeader("set-cookie", "loggedIn=true");
   res.redirect("/");
 };
--- dist/controllers/auth.js
+++ dist/controllers/auth.js
@@ -1,12 +1,14 @@
 export const getLogin = (req, res, next) => {
+    var _a;
+    const isLoggedIn = ((_a = req.get("Cookie")) === null || _a === void 0 ? void 0 : _a.split("=")[1]) === "true";
     res.render("auth/login", {
         pageTitle: "Login",
         path: "/login",
-        isAuthenticated: false,
+        isAuthenticated: isLoggedIn,
     });
 };
 export const postLogin = (req, res, next) => {
-    req.isLoggedIn = true;
+    res.setHeader("set-cookie", "loggedIn=true");
     res.redirect("/");
 };
 //# sourceMappingURL=auth.js.map
