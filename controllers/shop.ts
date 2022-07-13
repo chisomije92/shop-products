cdc702da3c87e70d0c2044e71ce07350f7b7ca5f
Chisom <chisomije92@gmail.com>
2022-07-14 00:01:44 +0100

    implement async-await in shop controllers

--- controllers/shop.ts
+++ controllers/shop.ts
@@ -10,7 +10,7 @@ import { CustomError } from "../utils/custom-err.js";
 
 const ITEMS_PER_PAGE = 2;
 
-export const getProducts = (
+export const getProducts = async (
   req: Request,
   res: Response,
   next: NextFunction
@@ -21,110 +21,110 @@ export const getProducts = (
   }
   const page: number = queryPage || 1;
 
-  let totalItems: number;
-  Product.find()
-    .countDocuments()
-    .then((numProducts) => {
-      totalItems = numProducts;
-      return Product.find()
-        .skip((page - 1) * ITEMS_PER_PAGE)
-        .limit(ITEMS_PER_PAGE)
-        .then((products) => {
-          res.render("shop/product-list", {
-            products: products,
-            pageTitle: "Products",
-            path: "/products",
-            totalItems: totalItems,
-            currentPage: page,
-            hasNextPage: ITEMS_PER_PAGE * page < totalItems,
-            hasPreviousPage: page > 1,
-            nextPage: page + 1,
-            previousPage: page - 1,
-            lastPage: Math.ceil(numProducts / ITEMS_PER_PAGE),
-            numProducts: numProducts,
-          });
-        });
-    })
-    .catch((err: Error) => {
-      return next(new CustomError(err.message, 500));
+  try {
+    const totalDocuments = await Product.find().countDocuments();
+    const products = await Product.find()
+      .skip((page - 1) * ITEMS_PER_PAGE)
+      .limit(ITEMS_PER_PAGE);
+    res.render("shop/product-list", {
+      products: products,
+      pageTitle: "Products",
+      path: "/products",
+      totalItems: totalDocuments,
+      currentPage: page,
+      hasNextPage: ITEMS_PER_PAGE * page < totalDocuments,
+      hasPreviousPage: page > 1,
+      nextPage: page + 1,
+      previousPage: page - 1,
+      lastPage: Math.ceil(totalDocuments / ITEMS_PER_PAGE),
+      numProducts: totalDocuments,
     });
+  } catch (err: any) {
+    next(new CustomError(err.message, 500));
+  }
 };
 
-export const getProduct = (req: Request, res: Response, next: NextFunction) => {
+export const getProduct = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then((product) => {
-      res.render("shop/product-detail", {
-        product: product,
-        pageTitle: product?.title,
-        path: "/products",
-        isAuthenticated: req.session.isLoggedIn,
-      });
-    })
-    .catch((err: Error) => {
-      return next(new CustomError(err.message, 500));
+  try {
+    const product = await Product.findById(prodId);
+    res.render("shop/product-detail", {
+      product: product,
+      pageTitle: product?.title,
+      path: "/products",
+      isAuthenticated: req.session.isLoggedIn,
     });
+  } catch (err: any) {
+    next(new CustomError(err.message, 500));
+  }
 };
 
-export const getIndex = (req: Request, res: Response, next: NextFunction) => {
+export const getIndex = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   let queryPage;
   if (req.query && req.query.page) {
     queryPage = +req.query.page;
   }
   const page: number = queryPage || 1;
 
-  let totalItems: number;
-  Product.find()
-    .countDocuments()
-    .then((numProducts) => {
-      totalItems = numProducts;
-      return Product.find()
-        .skip((page - 1) * ITEMS_PER_PAGE)
-        .limit(ITEMS_PER_PAGE)
-        .then((products) => {
-          res.render("shop/index", {
-            products: products,
-            pageTitle: "Shop",
-            path: "/",
-            totalItems: totalItems,
-            currentPage: page,
-            hasNextPage: ITEMS_PER_PAGE * page < totalItems,
-            hasPreviousPage: page > 1,
-            nextPage: page + 1,
-            previousPage: page - 1,
-            lastPage: Math.ceil(numProducts / ITEMS_PER_PAGE),
-            numProducts: numProducts,
-          });
-        });
-    })
-    .catch((err: Error) => {
-      return next(new CustomError(err.message, 500));
+  try {
+    const totalDocuments = await Product.find().countDocuments();
+    const products = await Product.find()
+      .skip((page - 1) * ITEMS_PER_PAGE)
+      .limit(ITEMS_PER_PAGE);
+
+    res.render("shop/index", {
+      products: products,
+      pageTitle: "Shop",
+      path: "/",
+      totalItems: totalDocuments,
+      currentPage: page,
+      hasNextPage: ITEMS_PER_PAGE * page < totalDocuments,
+      hasPreviousPage: page > 1,
+      nextPage: page + 1,
+      previousPage: page - 1,
+      lastPage: Math.ceil(totalDocuments / ITEMS_PER_PAGE),
+      numProducts: totalDocuments,
     });
+  } catch (err: any) {
+    next(new CustomError(err.message, 500));
+  }
 };
 
-export const getCart = (req: Request, res: Response, next: NextFunction) => {
-  req.user
-    ?.populate("cart.items.productId")
-    .then((user: UserType) => {
-      const products = user.cart.items;
-      res.render("shop/cart", {
-        path: "/cart",
-        pageTitle: "Your Cart",
-        products: products,
-      });
-    })
-    .catch((err: Error) => next(new CustomError(err.message, 500)));
+export const getCart = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const products = await req.user?.populate("cart.items.productId");
+    res.render("shop/cart", {
+      path: "/cart",
+      pageTitle: "Your Cart",
+      products: products,
+    });
+  } catch (err: any) {
+    next(new CustomError(err.message, 500));
+  }
 };
 
-export const postCart = (req: Request, res: Response, next: NextFunction) => {
+export const postCart = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const prodId: string = req.body.productId;
-  Product.findById(prodId)
-    .then((product) => {
-      return req.user?.addToCart(product);
-    })
-    .then(() => {
-      res.redirect("/cart");
-    });
+  const product = await Product.findById(prodId);
+  await req.user?.addToCart(product);
+
+  res.redirect("/cart");
 };
 
 export const deleteCartProduct = (
@@ -133,142 +133,140 @@ export const deleteCartProduct = (
   next: NextFunction
 ) => {
   const prodId: string = req.body.productId;
-  req.user
-    ?.deleteItemFromCart(prodId)
-    .then(() => {
-      res.redirect("/cart");
-    })
-    .catch((err: Error) => {
-      {
-        return next(new CustomError(err.message, 500));
-      }
-    });
+  try {
+    req.user?.removeFromCart(prodId);
+    res.redirect("/cart");
+  } catch (err: any) {
+    next(new CustomError(err.message, 500));
+  }
 };
 
-export const getOrders = (req: Request, res: Response, next: NextFunction) => {
-  Order.find({ "user.userId": req.user?._id })
-    .then((orders) => {
-      res.render("shop/orders", {
-        orders: orders,
-        pageTitle: "Your Orders",
-        path: "/orders",
-      });
-    })
-    .catch((err: Error) => {
-      return next(new CustomError(err.message, 500));
-    });
-};
-
-export const getCheckout = (
+export const getOrders = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  req.user
-    ?.populate("cart.items.productId")
-    .then((user: UserType) => {
-      const products = user.cart.items;
-
-      let totalPrice = 0;
-      products.forEach((product: any) => {
-        totalPrice += product.quantity * product.productId.price;
-      });
-      res.render("shop/checkout", {
-        path: "/checkout",
-        pageTitle: "Checkout",
-        products: products,
-        totalPrice: totalPrice,
-        userEmail: user.email,
-      });
-    })
-    .catch((err: Error) => {
-      return next(new CustomError(err.message, 500));
+  try {
+    const orders = await Order.find({ "user.userId": req.user?._id });
+    res.render("shop/orders", {
+      orders: orders,
+      pageTitle: "Your Orders",
+      path: "/orders",
     });
+  } catch (err: any) {
+    next(new CustomError(err.message, 500));
+  }
 };
 
-export const verifyOrder = (
+export const getCheckout = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = await req.user?.populate("cart.items.productId");
+    const products = user.cart.items;
+    let totalPrice = 0;
+    products.forEach((product: any) => {
+      totalPrice += product.quantity * product.productId.price;
+    });
+
+    res.render("shop/checkout", {
+      path: "/checkout",
+      pageTitle: "Checkout",
+      products: products,
+      totalPrice: totalPrice,
+      userEmail: user.email,
+    });
+  } catch (err: any) {
+    next(new CustomError(err.message, 500));
+  }
+};
+
+export const verifyOrder = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   const reference = req.query.reference;
 
-  req.user
-    ?.populate("cart.items.productId")
-    .then((user: any) => {
-      const products = user?.cart.items.map((i: any) => {
-        return {
-          quantity: i.quantity,
-          product: { ...i.productId._doc },
-        };
-      });
-      const order = new Order({
-        user: {
-          email: req.user?.email,
-          userId: req.user?._id,
-        },
-        products: products,
-      });
-      return order.save();
-    })
-    .then(() => {
-      req.user?.clearCart();
-    })
-    .then(() => {
-      return fetch(`https://api.paystack.co/transaction/verify/${reference}`, {
+  try {
+    const user = await req.user?.populate("cart.items.productId");
+
+    const products = user?.cart.items.map((i: any) => {
+      return {
+        quantity: i.quantity,
+        product: { ...i.productId._doc },
+      };
+    });
+    const order = new Order({
+      user: {
+        email: req.user?.email,
+        userId: req.user?._id,
+      },
+      products: products,
+    });
+    await order.save();
+
+    req.user?.clearCart();
+
+    const response = await fetch(
+      `https://api.paystack.co/transaction/verify/${reference}`,
+      {
         method: "GET",
         headers: {
           authorization: `bearer ${process.env.PAYSTACK_KEY}`,
         },
-      });
-    })
-    .then((response: any) => {
-      return response.json();
-    })
-    .then((data: any) => {
-      res.status(200).json(data);
-    })
-    .catch((err: Error) => {
-      return next(new CustomError(err.message, 500));
-    });
+      }
+    );
+
+    const data = await response.json();
+
+    res.status(200).json(data);
+  } catch (err: any) {
+    next(new CustomError(err.message, 500));
+  }
 };
 
-export const getInvoice = (req: Request, res: Response, next: NextFunction) => {
-  const orderId = req.params.orderId;
-  Order.findById(orderId)
-    .then((order) => {
-      if (!order) {
-        return next(new Error("No order found."));
-      }
-      if (order.user.userId.toString() !== req.user?._id.toString()) {
-        return next(new Error("Unauthorized"));
-      }
-      const invoiceName = "invoice-" + orderId + ".pdf";
-      const invoicePath = path.join("data", "invoices", invoiceName);
-      const pdfDoc = new PDFDocument();
-      pdfDoc.pipe(fs.createWriteStream(invoicePath));
+export const getInvoice = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const orderId = req.params.orderId;
+    const order = await Order.findById(orderId);
 
-      pdfDoc.pipe(res);
-      pdfDoc.fontSize(26).text("Invoice", { underline: true });
-      pdfDoc.text("--------------------");
-      let totalPrice = 0;
-      order.products.forEach((product: any) => {
-        totalPrice += product.quantity * product.product.price;
-        pdfDoc
-          .fontSize(16)
-          .text(
-            product.product.title +
-              " - " +
-              product.quantity +
-              " x " +
-              product.product.price
-          );
-      });
-      pdfDoc.text("--------------------");
-      pdfDoc.fontSize(20).text("Total Price: $" + totalPrice);
-      pdfDoc.end();
-    })
-    .catch((err: Error) => {
-      return next(new CustomError(err.message, 500));
+    if (!order) {
+      return next(new Error("No order found."));
+    }
+    if (order.user.userId.toString() !== req.user?._id.toString()) {
+      return next(new Error("Unauthorized"));
+    }
+    const invoiceName = "invoice-" + orderId + ".pdf";
+    const invoicePath = path.join("data", "invoices", invoiceName);
+    const pdfDoc = new PDFDocument();
+    pdfDoc.pipe(fs.createWriteStream(invoicePath));
+    pdfDoc.pipe(res);
+    pdfDoc.fontSize(26).text("Invoice", { underline: true });
+    pdfDoc.text("--------------------");
+    let totalPrice = 0;
+    order.products.forEach((product: any) => {
+      totalPrice += product.quantity * product.product.price;
+      pdfDoc
+        .fontSize(16)
+        .text(
+          product.product.title +
+            " - " +
+            product.quantity +
+            " x " +
+            product.product.price
+        );
     });
+    pdfDoc.text("--------------------");
+    pdfDoc.fontSize(20).text("Total Price: $" + totalPrice);
+    pdfDoc.end();
+  } catch (err: any) {
+    next(new CustomError(err.message, 500));
+  }
 };
--- dist/controllers/shop.js
+++ dist/controllers/shop.js
@@ -1,3 +1,12 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import path from "path";
 import PDFDocument from "pdfkit";
 import fs from "fs";
@@ -6,139 +15,129 @@ import Order from "../models/order.js";
 import fetch from "node-fetch";
 import { CustomError } from "../utils/custom-err.js";
 const ITEMS_PER_PAGE = 2;
-export const getProducts = (req, res, next) => {
+export const getProducts = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     let queryPage;
     if (req.query && req.query.page) {
         queryPage = +req.query.page;
     }
     const page = queryPage || 1;
-    let totalItems;
-    Product.find()
-        .countDocuments()
-        .then((numProducts) => {
-        totalItems = numProducts;
-        return Product.find()
+    try {
+        const totalDocuments = yield Product.find().countDocuments();
+        const products = yield Product.find()
             .skip((page - 1) * ITEMS_PER_PAGE)
-            .limit(ITEMS_PER_PAGE)
-            .then((products) => {
-            res.render("shop/product-list", {
-                products: products,
-                pageTitle: "Products",
-                path: "/products",
-                totalItems: totalItems,
-                currentPage: page,
-                hasNextPage: ITEMS_PER_PAGE * page < totalItems,
-                hasPreviousPage: page > 1,
-                nextPage: page + 1,
-                previousPage: page - 1,
-                lastPage: Math.ceil(numProducts / ITEMS_PER_PAGE),
-                numProducts: numProducts,
-            });
+            .limit(ITEMS_PER_PAGE);
+        res.render("shop/product-list", {
+            products: products,
+            pageTitle: "Products",
+            path: "/products",
+            totalItems: totalDocuments,
+            currentPage: page,
+            hasNextPage: ITEMS_PER_PAGE * page < totalDocuments,
+            hasPreviousPage: page > 1,
+            nextPage: page + 1,
+            previousPage: page - 1,
+            lastPage: Math.ceil(totalDocuments / ITEMS_PER_PAGE),
+            numProducts: totalDocuments,
         });
-    })
-        .catch((err) => {
-        return next(new CustomError(err.message, 500));
-    });
-};
-export const getProduct = (req, res, next) => {
+    }
+    catch (err) {
+        next(new CustomError(err.message, 500));
+    }
+});
+export const getProduct = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     const prodId = req.params.productId;
-    Product.findById(prodId)
-        .then((product) => {
+    try {
+        const product = yield Product.findById(prodId);
         res.render("shop/product-detail", {
             product: product,
             pageTitle: product === null || product === void 0 ? void 0 : product.title,
             path: "/products",
             isAuthenticated: req.session.isLoggedIn,
         });
-    })
-        .catch((err) => {
-        return next(new CustomError(err.message, 500));
-    });
-};
-export const getIndex = (req, res, next) => {
+    }
+    catch (err) {
+        next(new CustomError(err.message, 500));
+    }
+});
+export const getIndex = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     let queryPage;
     if (req.query && req.query.page) {
         queryPage = +req.query.page;
     }
     const page = queryPage || 1;
-    let totalItems;
-    Product.find()
-        .countDocuments()
-        .then((numProducts) => {
-        totalItems = numProducts;
-        return Product.find()
+    try {
+        const totalDocuments = yield Product.find().countDocuments();
+        const products = yield Product.find()
             .skip((page - 1) * ITEMS_PER_PAGE)
-            .limit(ITEMS_PER_PAGE)
-            .then((products) => {
-            res.render("shop/index", {
-                products: products,
-                pageTitle: "Shop",
-                path: "/",
-                totalItems: totalItems,
-                currentPage: page,
-                hasNextPage: ITEMS_PER_PAGE * page < totalItems,
-                hasPreviousPage: page > 1,
-                nextPage: page + 1,
-                previousPage: page - 1,
-                lastPage: Math.ceil(numProducts / ITEMS_PER_PAGE),
-                numProducts: numProducts,
-            });
+            .limit(ITEMS_PER_PAGE);
+        res.render("shop/index", {
+            products: products,
+            pageTitle: "Shop",
+            path: "/",
+            totalItems: totalDocuments,
+            currentPage: page,
+            hasNextPage: ITEMS_PER_PAGE * page < totalDocuments,
+            hasPreviousPage: page > 1,
+            nextPage: page + 1,
+            previousPage: page - 1,
+            lastPage: Math.ceil(totalDocuments / ITEMS_PER_PAGE),
+            numProducts: totalDocuments,
         });
-    })
-        .catch((err) => {
-        return next(new CustomError(err.message, 500));
-    });
-};
-export const getCart = (req, res, next) => {
+    }
+    catch (err) {
+        next(new CustomError(err.message, 500));
+    }
+});
+export const getCart = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     var _a;
-    (_a = req.user) === null || _a === void 0 ? void 0 : _a.populate("cart.items.productId").then((user) => {
-        const products = user.cart.items;
+    try {
+        const products = yield ((_a = req.user) === null || _a === void 0 ? void 0 : _a.populate("cart.items.productId"));
         res.render("shop/cart", {
             path: "/cart",
             pageTitle: "Your Cart",
             products: products,
         });
-    }).catch((err) => next(new CustomError(err.message, 500)));
-};
-export const postCart = (req, res, next) => {
+    }
+    catch (err) {
+        next(new CustomError(err.message, 500));
+    }
+});
+export const postCart = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    var _b;
     const prodId = req.body.productId;
-    Product.findById(prodId)
-        .then((product) => {
-        var _a;
-        return (_a = req.user) === null || _a === void 0 ? void 0 : _a.addToCart(product);
-    })
-        .then(() => {
-        res.redirect("/cart");
-    });
-};
+    const product = yield Product.findById(prodId);
+    yield ((_b = req.user) === null || _b === void 0 ? void 0 : _b.addToCart(product));
+    res.redirect("/cart");
+});
 export const deleteCartProduct = (req, res, next) => {
     var _a;
     const prodId = req.body.productId;
-    (_a = req.user) === null || _a === void 0 ? void 0 : _a.deleteItemFromCart(prodId).then(() => {
+    try {
+        (_a = req.user) === null || _a === void 0 ? void 0 : _a.removeFromCart(prodId);
         res.redirect("/cart");
-    }).catch((err) => {
-        {
-            return next(new CustomError(err.message, 500));
-        }
-    });
+    }
+    catch (err) {
+        next(new CustomError(err.message, 500));
+    }
 };
-export const getOrders = (req, res, next) => {
-    var _a;
-    Order.find({ "user.userId": (_a = req.user) === null || _a === void 0 ? void 0 : _a._id })
-        .then((orders) => {
+export const getOrders = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    var _c;
+    try {
+        const orders = yield Order.find({ "user.userId": (_c = req.user) === null || _c === void 0 ? void 0 : _c._id });
         res.render("shop/orders", {
             orders: orders,
             pageTitle: "Your Orders",
             path: "/orders",
         });
-    })
-        .catch((err) => {
-        return next(new CustomError(err.message, 500));
-    });
-};
-export const getCheckout = (req, res, next) => {
-    var _a;
-    (_a = req.user) === null || _a === void 0 ? void 0 : _a.populate("cart.items.productId").then((user) => {
+    }
+    catch (err) {
+        next(new CustomError(err.message, 500));
+    }
+});
+export const getCheckout = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    var _d;
+    try {
+        const user = yield ((_d = req.user) === null || _d === void 0 ? void 0 : _d.populate("cart.items.productId"));
         const products = user.cart.items;
         let totalPrice = 0;
         products.forEach((product) => {
@@ -151,15 +150,16 @@ export const getCheckout = (req, res, next) => {
             totalPrice: totalPrice,
             userEmail: user.email,
         });
-    }).catch((err) => {
-        return next(new CustomError(err.message, 500));
-    });
-};
-export const verifyOrder = (req, res, next) => {
-    var _a;
+    }
+    catch (err) {
+        next(new CustomError(err.message, 500));
+    }
+});
+export const verifyOrder = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    var _e, _f, _g, _h;
     const reference = req.query.reference;
-    (_a = req.user) === null || _a === void 0 ? void 0 : _a.populate("cart.items.productId").then((user) => {
-        var _a, _b;
+    try {
+        const user = yield ((_e = req.user) === null || _e === void 0 ? void 0 : _e.populate("cart.items.productId"));
         const products = user === null || user === void 0 ? void 0 : user.cart.items.map((i) => {
             return {
                 quantity: i.quantity,
@@ -168,39 +168,35 @@ export const verifyOrder = (req, res, next) => {
         });
         const order = new Order({
             user: {
-                email: (_a = req.user) === null || _a === void 0 ? void 0 : _a.email,
-                userId: (_b = req.user) === null || _b === void 0 ? void 0 : _b._id,
+                email: (_f = req.user) === null || _f === void 0 ? void 0 : _f.email,
+                userId: (_g = req.user) === null || _g === void 0 ? void 0 : _g._id,
             },
             products: products,
         });
-        return order.save();
-    }).then(() => {
-        var _a;
-        (_a = req.user) === null || _a === void 0 ? void 0 : _a.clearCart();
-    }).then(() => {
-        return fetch(`https://api.paystack.co/transaction/verify/${reference}`, {
+        yield order.save();
+        (_h = req.user) === null || _h === void 0 ? void 0 : _h.clearCart();
+        const response = yield fetch(`https://api.paystack.co/transaction/verify/${reference}`, {
             method: "GET",
             headers: {
                 authorization: `bearer ${process.env.PAYSTACK_KEY}`,
             },
         });
-    }).then((response) => {
-        return response.json();
-    }).then((data) => {
+        const data = yield response.json();
         res.status(200).json(data);
-    }).catch((err) => {
-        return next(new CustomError(err.message, 500));
-    });
-};
-export const getInvoice = (req, res, next) => {
-    const orderId = req.params.orderId;
-    Order.findById(orderId)
-        .then((order) => {
-        var _a;
+    }
+    catch (err) {
+        next(new CustomError(err.message, 500));
+    }
+});
+export const getInvoice = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    var _j;
+    try {
+        const orderId = req.params.orderId;
+        const order = yield Order.findById(orderId);
         if (!order) {
             return next(new Error("No order found."));
         }
-        if (order.user.userId.toString() !== ((_a = req.user) === null || _a === void 0 ? void 0 : _a._id.toString())) {
+        if (order.user.userId.toString() !== ((_j = req.user) === null || _j === void 0 ? void 0 : _j._id.toString())) {
             return next(new Error("Unauthorized"));
         }
         const invoiceName = "invoice-" + orderId + ".pdf";
@@ -224,9 +220,9 @@ export const getInvoice = (req, res, next) => {
         pdfDoc.text("--------------------");
         pdfDoc.fontSize(20).text("Total Price: $" + totalPrice);
         pdfDoc.end();
-    })
-        .catch((err) => {
-        return next(new CustomError(err.message, 500));
-    });
-};
+    }
+    catch (err) {
+        next(new CustomError(err.message, 500));
+    }
+});
 //# sourceMappingURL=shop.js.map
